abdecb79e7540750cd4fcd5f62727fc8c9e323cb
patel <patel@codialist.de>
2025-11-03 20:10:17 +0100

    s
    more changes

--- cmake/deps.stm32cubef4.cmake
+++ cmake/deps.stm32cubef4.cmake
@@ -1,6 +1,9 @@
-# dependency on the STM32CubeF4 firmware is brought by CPM as a package
-CPMAddPackage (
-    NAME        STM32CubeF4
-    URL         https://github.com/STMicroelectronics/STM32CubeF4/archive/refs/tags/${GITHUB_BRANCH_stm32cubef4}.tar.gz
-    URL_HASH    SHA256=c4d121386fc1b0d6566c4ae9e08af14b24eea089250a0e86ff341dcf7ddb256e
-)
+
+# Remove the first character from GITHUB_BRANCH_stm32cubef4 and store in versionxx
+string(SUBSTRING ${GITHUB_BRANCH_stm32cubef4} 1 -1 versionxx)
+
+set (GITHUB_BRANCH_stm32cubexx ${versionxx} CACHE STRING "GitHub branch for stm32cubexx")
+
+set (GITHUB_BRANCH_stm32cubexx_SHA ${GITHUB_BRANCH_stm32cubef4_SHA256} CACHE STRING "GitHub branch SHA256 for stm32cubexx")
+
+include (${cmake_scripts_SOURCE_DIR}/silicon/st/stm32cubexx.cmake)
